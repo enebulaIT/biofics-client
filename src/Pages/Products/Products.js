ce4d0ddcce03260f6976971756cf170f3e31b9e9
Anirudha sah <asah@wagepoint.com>
2022-05-17 22:48:42 +0530

    changes

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -1,6 +1,54 @@
+import { useEffect, useState } from 'react';
+import Hero from '../../Components/Hero/Hero';
+import PageLoader from '../../Components/PageLoader/PageLoader';
+import api from '../../Api/publicApi';
+import SingleProduct from './SingleProduct/SingleProduct';
+import classes from './Products.module.css';
+
 const Products = () => {
+
+    const [loading, setLoading] = useState(false);
+    const [productsData, setProductData] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await api.get(`/api/products?populate=*`);
+                setProductData(response.data.data);
+            } catch (err) {
+                console.log({ ...err });
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchData();
+    }, []);
+
+    const getProductElements = () => {
+        const elements = [];
+
+        productsData.forEach(product => {
+            elements.push(
+                <SingleProduct key = {product.id} productData = {product}/>
+            );
+        });
+
+        return elements;
+    }
+
     return (
-        <div>Products</div>
+        <div className={classes.container}>
+            {loading && <PageLoader />}
+
+            <div className={classes.banner}>
+                <Hero title='Our Products' />
+            </div>
+
+            <div className={classes.productsWrapper}>
+                {getProductElements()}
+            </div>
+        </div>
     )
 }
 
