a5981fa2d5d44d524af39ef5220571ee4499ac5b
Anirudha sah <asah@wagepoint.com>
2023-02-08 21:03:41 +0530

    api changes

--- src/Api/publicApi.js
+++ src/Api/publicApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_STRAPI_BASE_URL 
+    baseURL: "https://biofics-admin.onrender.com"
 });
 
 export default api;
